64b32a3c206ba52fe3abd52e6d148d7e6260dbdc
AlikhanSagatbekov <221112@astana.edu.kz>
2023-04-09 21:44:42 +0600

    Fibonacci sequence using recursion

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,6 @@ import java.util.Scanner;
 
 public class Main {
     static Scanner sc = new Scanner(System.in);
-
-    //Main function for testing the solutions
     public static void main(String[] args) {
 
 //        int arr[] = {10,1,32,3,45};
@@ -22,57 +20,52 @@ public class Main {
 
 //        System.out.println(factorial(5));
     }
-
-    // Declaring a function
     static double findAverage(double sum, int n, int arr[]) {
-        // Check if we've processed all elements in the array
         if (n == arr.length) {
-            // If we have, return the average by dividing the sum by the length of the array
             return sum / arr.length;
         }
-
-        // If there are still elements to process, add the current element to the sum and recurse
         return findAverage(sum + arr[n], n + 1, arr);
     }
 
-
-    //This is the fucntion to find the minimum element in the array
-    //recursively
-    static int findMin(int min, int n, int arr[]) { // the program starts with the min or first element
-        if (n == arr.length) { // The condition to exit recursion
-            return min; // returning the current minimum number
+    static int findMin(int min, int n, int arr[]) {
+        if (n == arr.length) {
+            return min;
         }
 
-        if (min > arr[n]) { // we go through the every element of the array
-            return findMin(arr[n], n + 1, arr);  // if the element is lower than we take it as minimum
+        if (min > arr[n]) {
+            return findMin(arr[n], n + 1, arr);
         } else {
-            return findMin(min, n + 1, arr); // if
+            return findMin(min, n + 1, arr);
         }
 
     }
 
     // Declaring a function
-    static boolean isPrime(int n, int temp) { // It is important to start from 2 when we are dealing with this function
-        if (n <= 2 && n > 0) // if the number is too small the program exits
+    static boolean isPrime(int n, int temp) {
+        if (n <= 2 && n > 0)
             return true;
-        if (n % temp == 0) { // If the number can be divided without left over than it is not Prime (exit condition)
+        if (n % temp == 0) {
             return false;
         }
-        if (temp * temp > n) { // the second exit condition because mathematically proven that all the numbers that can divide a certain number lie before the square of N
-            return true;
+        if (temp * temp > n) {
         }
-        temp++;// to go through all the numbers inside
-        return isPrime(n, temp); // calling the function for every temporary number
+        temp++;
+        return isPrime(n, temp);
     }
-
-    // Declare a function named "factorial" that takes in an integer "n" as an argument
     static int factorial(int n) {
         // Check if n is less than or equal to 0
         if (n <= 0) {
-            // If it is, return 1 (since 0! and negative numbers are not defined)
             return 1;
         }
-        // If n is greater than 0, recursively call the factorial function with n-1 and multiply it by n
         return n * factorial(n - 1);
+    }
+    static int fibonacci(int n){
+        if (n <= 0){
+            return 0;
+        }
+        else if (n == 1){
+            return 1;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
     }
 }
